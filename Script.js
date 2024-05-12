8a56f12b432d3cd7b59e83e3339e768157a5ebb0
“Rohit <hemlatamehra80@gmail.com>
2024-05-12 20:48:32 +0530

    initial commit

---
+++ Script.js
@@ -0,0 +1,66 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+
+const app = express();
+
+// URLs of the websites you want to scrape
+const urls = [
+  'https://www.google.com/finance/quote/ZOMATO:NSE',
+  'https://www.google.com/finance/quote/META:NASDAQ',
+  'https://www.google.com/finance/quote/COIN:NASDAQ',
+  'https://www.google.com/finance/quote/GOOGL:NASDAQ',
+  'https://www.google.com/finance/quote/INFY:NYSE',
+  'https://www.google.com/finance/quote/INDIANB:NSE',
+  'https://www.google.com/finance/quote/INDIACEM:NSE'
+];
+
+// Function to fetch HTML content of a URL
+async function fetchData(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.log('Error fetching data:', error);
+    return null;
+  }
+}
+
+// Function to scrape data from a given HTML
+function scrapeData(html) {
+  const $ = cheerio.load(html);
+  const elementsWithData = [];
+  $('.YMlKec.fxKbKc').each((index, element) => {
+    elementsWithData.push($(element).text());
+  });
+  return elementsWithData;
+}
+
+// Function to scrape data from multiple URLs
+async function scrapeAllData() {
+  const scrapedData = [];
+  for (const url of urls) {
+    const html = await fetchData(url);
+    if (html) {
+      const data = scrapeData(html);
+      scrapedData.push({ url, data });
+    }
+  }
+  return scrapedData;
+}
+
+// Route handler to send scraped data to the /api/data route
+app.get('/api/data', async (req, res) => {
+  try {
+    const data = await scrapeAllData();
+    res.json(data);
+    console.log('Data sent to /api/data route');
+  } catch (error) {
+    console.error('Error sending data to /api/data route:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.listen(3001, () => {
+  console.log(`Server is running at http://localhost:3001`);
+});
